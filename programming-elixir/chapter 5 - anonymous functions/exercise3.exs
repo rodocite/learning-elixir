46342974e43ce2e5307094bd897ccce0c51be659
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-05 17:06:14 -0700

    Exercise 3 solution
    Complete fizz_buzz solution with no conditionals. Note 4th pattern
    added to fizz_buzz_handler.

---
+++ programming-elixir/chapter 5 - anonymous functions/exercise3.exs
@@ -0,0 +1,18 @@
+fizz_buzz_handler = fn
+  (0, 0, _) -> "FizzBuzz"
+  (0, _, _) -> "Fizz"
+  (_, 0, _) -> "Buzz"
+  (_, _, n) -> n
+end
+
+fizz_buzz = fn (n) ->
+  fizz_buzz_handler.(rem(n, 3), rem(n, 5), n)
+end
+
+IO.puts(fizz_buzz.(10))
+IO.puts(fizz_buzz.(11))
+IO.puts(fizz_buzz.(12))
+IO.puts(fizz_buzz.(13))
+IO.puts(fizz_buzz.(14))
+IO.puts(fizz_buzz.(15))
+IO.puts(fizz_buzz.(16))
